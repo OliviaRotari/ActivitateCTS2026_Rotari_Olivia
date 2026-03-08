2642e0a3136a3c4585319c67cbcf086d9ba86a1e
Olivia <olivia.rotari2602@gmail.com>
2026-03-08 21:58:25 +0200

    Seminar 3

--- Seminar2/src/ro/ase/cts/program/Program.java
+++ Seminar2/src/ro/ase/cts/program/Program.java
@@ -7,19 +7,19 @@ import ro.ase.cts.loaders.AplicantiLoader;
 import java.io.FileNotFoundException;
 import java.util.List;
 
-
-
 public class Program {
 
 	public static void main(String[] args) {
 		List<Aplicant> listaAngajati;
 		try {
-			AplicantiLoader loader=new AngajatiLoader();
+			AplicantiLoader loader = new AngajatiLoader();
 			listaAngajati = loader.loadAplicants("angajati.txt");
-			for(Aplicant angajat:listaAngajati)
+			for (Aplicant angajat : listaAngajati) {
 				System.out.println(angajat.toString());
+				angajat.afisareFinantare();
+				angajat.afisareStatut();
+			}
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
