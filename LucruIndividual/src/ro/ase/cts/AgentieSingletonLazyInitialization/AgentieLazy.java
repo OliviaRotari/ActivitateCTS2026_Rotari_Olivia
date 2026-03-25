debe56dc08adc32798d1b4444c875fd7348f1560
Olivia <olivia.rotari2602@gmail.com>
2026-03-25 12:12:42 +0200

    Lucru individual Singleton

--- LucruIndividual/src/ro/ase/cts/AgentieSingletonLazyInitialization/AgentieLazy.java
+++ LucruIndividual/src/ro/ase/cts/AgentieSingletonLazyInitialization/AgentieLazy.java
@@ -3,4 +3,5 @@ package ro.ase.cts.AgentieSingletonLazyInitialization;
 public class AgentieLazy {
     private String numeAgentie;
     private int nrPacheteVandute;
+    private static AgentieLazy instance=null;
 }
